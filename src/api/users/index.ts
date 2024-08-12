f008977d14496764f2153dc01f4f1f43a24a54cf
wb_liufuyuan <wb_liufuyuan@kuaishou.com>
2024-08-12 21:08:27 +0800

    feat: add code .

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,4 +1,4 @@
-import { request } from "@/utils/service"
+import { request, requestV1 } from "@/utils/service"
 import type * as Users from "./types/users"
 
 /** 获取指定用户的会话列表接口 */
@@ -6,5 +6,23 @@ export function usersUserIdConversations(user_id: string) {
   return request<Users.UsersUserIdConversationsResponseData[]>({
     url: `/api/users/${user_id}/conversations`,
     method: "get"
+  })
+}
+
+/** 注册接口 */
+export function usersRegister(userInfo: Users.UsersRegisterRequestData) {
+  return requestV1<Users.UsersRegisterResponseData>({
+    url: "/api/users/register",
+    method: "post",
+    params: userInfo
+  })
+}
+
+/** 登陆接口 */
+export function usersLogin(userInfo: Users.UsersLoginRequestData) {
+  return requestV1<Users.UsersLoginResponseData>({
+    url: "/api/users/login",
+    method: "post",
+    params: userInfo
   })
 }
--- src/api/users/types/users.ts
+++ src/api/users/types/users.ts
@@ -4,3 +4,28 @@ export interface UsersUserIdConversationsResponseData {
   chat_type: string // 会话类型
   create_time: string
 }
+
+// 注册接口入参
+export interface UsersRegisterRequestData {
+  username: string
+  password: string
+}
+
+export interface UsersRegisterResponseData {
+  id: string
+  username: string
+}
+
+/**
+ * 登陆接口出入参
+ */
+export interface UsersLoginRequestData {
+  username: string
+  password: string
+}
+
+export interface UsersLoginResponseData {
+  id: string
+  username: string
+  message: string
+}
